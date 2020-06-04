0fa57920d054202b21f7f55a211cc811e3341605
Anton Lysenko <adlysenko@edu.hse.ru>
2020-06-04 16:44:07 +0300

    Author - Lysenko Anton

--- newgame.py
+++ newgame.py
@@ -19,7 +19,16 @@ score = 0
 i = 0
 n = 0
 
+class shoot():
+    def __init__(self, x, y, radius, color, vel):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.vel = vel
 
+    def draw(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
 class player():
     def __init__(self, x, y, width, height):
@@ -38,6 +47,13 @@ class player():
         win.blit(fly, (self.x, self.y))
         self.hitbox = (self.x, self.y, self.width, self.height)
         pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+        self.count += 1
+        if self.count % 20 == 0:
+            bullets1.append(
+                shoot((round(self.x + self.width // 2)), round((self.y + self.height // 2) - 25), 3,
+                      (0, 255, 0), 20))
+            self.count = 0
+        win.blit(fly, (self.x, self.y))
         pygame.draw.rect(win, (255, 0, 0), (self.hitbox[0], self.hitbox[1] - 7, 40, 5))
         pygame.draw.rect(win, (0, 100, 0), (self.hitbox[0], self.hitbox[1] - 7, self.health//2.5, 5))
 
@@ -55,6 +71,7 @@ class enemy(object):
         self.vel = 2
         self.r = 200
         self.r1 = 148
+        self.count = 0
         self.hitbox = (self.x, self.y, self.width, self.height)
         self.health = 10
         self.visible = True
@@ -67,6 +84,12 @@ class enemy(object):
             pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
             pygame.draw.rect(win, (255, 0, 0), (self.hitbox[0], self.hitbox[1] - 7, 40, 5))
             pygame.draw.rect(win, (0, 100, 0), (self.hitbox[0], self.hitbox[1] - 7, 40 - (4 * (10 - self.health)), 5))
+            self.count += 1
+            if self.count % 60 == 0:
+                bullets2.append(
+                    shoot((round(self.x + self.width // 2)), round((self.y + self.height // 2) + 25), 3,
+                          (255, 0, 0), -5))
+                self.count = 0
 
     def hit(self):
         print("hit")
@@ -100,6 +123,10 @@ def drawWindow():
     plane.draw(win)
     for enem in enemyspot:
         enem.draw(win)
+    for bullet in bullets1:
+        bullet.draw(win)
+    for bullet in bullets2:
+        bullet.draw(win)
     text = font.render("Score: " + str(int(score)), 1, (255, 255, 255))
     win.blit(text, (315, 5))
     pygame.display.update()
@@ -107,7 +134,8 @@ def drawWindow():
 
 plane = player(180, 450, 38, 38)
 enemyspot = []
-
+bullets1 = []
+bullets2 = []
 
 
 
@@ -124,6 +152,30 @@ while run:
     if len(enemyspot) < 5:
         r3 = random.randrange(100, 200, 20)
         enemyspot.append(enemy(r3, -200, 40, 40, 450))
+    for bullet in bullets2:
+        if bullet.y < 600 and bullet.y > 0:
+            bullet.y -= bullet.vel
+        elif len(bullets2) > 1:
+            bullets2.pop(bullets2.index(bullet))
+        if bullet.x + bullet.radius > plane.hitbox[0] and bullet.x - bullet.radius < plane.hitbox[0] + plane.hitbox[
+            2]:
+            if bullet.y - bullet.radius < plane.hitbox[1] + plane.hitbox[3] and bullet.y + bullet.radius > \
+                    plane.hitbox[1]:
+                plane.hitme()
+                bullets2.pop(bullets2.index(bullet))
+    for bullet in bullets1:
+        if bullet.y < 600 and bullet.y > 0:
+            bullet.y -= bullet.vel
+        elif len(bullets1) > 1:
+            bullets1.pop(bullets1.index(bullet))
+        for enem in enemyspot:
+            if bullet.x + bullet.radius > enem.hitbox[0] and bullet.x - bullet.radius < enem.hitbox[0] + \
+                    enem.hitbox[2]:
+                if bullet.y - bullet.radius < enem.hitbox[1] + enem.hitbox[3] and bullet.y + bullet.radius > \
+                        enem.hitbox[1]:
+                    enem.hit()
+                    if bullet in bullets1:
+                        bullets1.pop(bullets1.index(bullet))
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and plane.x > 5:
         plane.x -= plane.vel
